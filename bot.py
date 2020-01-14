7c66958a0beb41a4d48faabac83daf61afac8d19
Vadim <vadimfe5@tuta.io>
2020-01-14 13:06:29 +0300

    got some problems with telegram api

--- bot.py
+++ bot.py
@@ -6,66 +6,122 @@ import time
 
 
 with open('tok.conf', 'r') as config:
-    token = config.read().split('|')[0]
+    token = config.read().split('|')[1]
     print(token)
 
 
 class Bot():
-    
+
+
     def __init__(self, tok):
         self.count_msg = False
         self.choice_msg = False
+        self.endl_msg = False
+        self.numb_msg = False
+        self.numb2_msg = False
         self.repeat_choice_msg = False
         self.repeat_count_msg = False
+        self.repeat_endl_msg = False
+        self.repeat_numb_msg = False
+        self.repeat_numb2_msg = False
         self.url = "https://api.telegram.org/bot{}".format(tok)
 
+
     def readmsg(self):
-        self.requp = self.url + "/getupdates?"
+        self.requp = self.url + "/getupdates"
         self.msgreq = requests.get(self.requp)
-        self.msg = self.msgreq.json().get('result')
-        self.lmsg = self.msg[-1]
+        self.lmsg = self.msgreq.json().get('result')
         self.readlm = str(self.lmsg).lower()
-        if self.count_msg is False: 
-            self.count()
+
+
+    def sendmsg(self, msg):
+        self.chat_id = re.search(r"\'chat\'\:\s\{\'id\'\:\s([0-9]{8,12})", self.readlm).group(1)
+        self.reqms = self.url + '/sendmessage?text={}&chat_id={}'.format(msg, self.chat_id)
+        requests.get(self.reqms)
+
 
     def count(self):
-        self.start = self.readlm.find('/count')
-        if self.count_msg is False and self.repeat_count_msg is False:
+        self.readmsg()
+        if self.repeat_count_msg is False:
             self.sendmsg('Hello! Type /count to practice in counting!')
             self.repeat_count_msg = True
-        if self.start == -1:
-            pass
-        elif self.start != -1 and self.count_msg is False:
+            print(self.readmsg)
+        if re.search(r'\/count', self.readlm):
             self.sendmsg("Started counting!")
             self.count_msg = True
             self.choice()
         if self.count_msg is False:
             time.sleep(1)
-            self.readmsg()
             self.count()
 
+
     def choice(self):
-        if self.choice_msg is False and self.repeat_choice_msg is False:
+        self.readmsg()
+        if self.repeat_choice_msg is False:
             self.sendmsg("Do you want a division test or multiplication? (/mul or /div):")
             self.repeat_choice_msg = True
-        if (self.readlm.find('/mul') != -1 or self.readlm.find('/div') != -1) and self.repeat_choice_msg is False:
-            self.sendmsg("Okay, that's it") 
-        if self.readlm.find('/mul') != -1:
+        if re.search(r'\/mul', self.readlm) is not None:
             self.sendmsg("Multiplication is chosen")
             self.choice_msg = True
-        if self.readlm.find('/div') != -1:
+            self.chosen = "mul"
+            self.endl()
+        elif re.search(r'\/div', self.readlm) is not None:
             self.sendmsg("Division is chosen")
             self.choice_msg = True
+            self.chosen = "div"
+            self.endl()
         if self.choice_msg is False:
             time.sleep(1)
-            self.readmsg()
             self.choice()
 
-    def sendmsg(self, msg):
-        self.chat_id = re.search(r"\'chat\'\:\s\{\'id\'\:\s([0-9]{10})", self.readlm).group(1)
-        self.reqms = self.url + '/sendmessage?text={}&chat_id={}'.format(msg, self.chat_id)
-        requests.get(self.reqms)
+
+    def endl(self):
+        self.readmsg()
+        if self.repeat_endl_msg is False:
+            self.sendmsg("Do you want to be in endless mathematical loop? (/yes or /no)")
+            self.repeat_endl_msg = True
+        if re.search(r'\/yes', self.readlm) is not None:
+            self.sendmsg('Loop mode is chosen')
+            self.endl_msg = True
+            self.numb2()
+        elif re.search(r'\/no', self.readlm) is not None:
+            self.sendmsg('Finite mode is chosen')
+            self.endl_msg = True
+            self.numb()
+        if self.endl_msg is False:
+            time.sleep(1)
+            self.endl()
+
+
+    def numb(self):
+        self.readmsg()
+        self.rpass = re.search(r'\/d([0-9]{1,6})', self.readlm)
+        if self.repeat_numb_msg is False:
+            self.sendmsg('How many iterations do you want before increasing difficulty? (/d\{num\}):')
+            self.repeat_numb_msg = True
+        if self.rpass is not None:
+            self.sendmsg('Have chosen {} iterations mode'.format(self.rpass.group(1)))
+            self.numb_msg = True
+            self.numb2()
+        if self.numb_msg is False:
+            time.sleep(1)
+            self.numb()
+
+
+    def numb2(self):
+        self.readmsg()
+        self.nitera = re.search(r'\/t([0-9]{1,6})', self.readlm)
+        if self.numb2_msg is False and self.repeat_numb2_msg is False:
+            self.sendmsg('How many iterations do you want to pass? (/t\{num\})')
+            self.repeat_numb2_msg = True
+        if self.nitera is not None:
+            self.sendmsg('You have chosen {} iterations total'.format(self.nitera.group(1)))
+            self.numb2_msg = True
+        if self.numb2_msg is False:
+            time.sleep(1)
+            self.numb2()
+
+
 
 pbot = Bot(token)
-while True:
-    pbot.readmsg()
+pbot.count()
