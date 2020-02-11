d38f874c6103465913ae466fe420e1828ec1b086
Vadim <vadimfe5@tuta.io>
2020-02-11 21:12:02 +0300

    modified for more stable ping test

--- cbot.sh
+++ cbot.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 echo "Start of cbot"
-tgerr = false
+tgerr=false
 
-xfce4-terminal -e /home/devbr/Documents/Projects/trachtenberg-sci/__cbot__/cbot.exe
+xfce4-terminal -e "./cbot.exe"
 
 while true; do
-        if [ -z `fping "api.telegram.org" | grep "alive"` ]; then
-                printf "\nConnection was lost! Now waiting 5s and trying to restart cbot!"
-                sleep 10
-		tgerr = true
-	if [ "$tgerr" == false ]; then
-		if [ `date | cut -d" " -f 5 | cut -d":" -f 2` == "00" ]; then
-			killall cbot.exe && xfce4-terminal -e "./cbot.exe"
+        if [ $((`ping api.telegram.org -q -c 10 | sed -n '4p' | cut -d' ' -f 4` -ge 5)) ]; then
+		printf "\nConnection was lost! Now waiting 10 seconds and trying to restart cbot!"
+		sleep 10
+		tgerr=true
+	else:
+		if [ "$tgerr" = false ]; then
+			if [ `date | cut -d" " -f 5 | cut -d":" -f 2` == "00" ]; then
+				killall cbot.exe && xfce4-terminal -e "./cbot.exe"
+			fi
+		elif [ "$tgerr" = true ]; then
+			killall cbot.exe && xfce4-terminal -e "./cbot.exe" && tgerr=false
 		fi
-	elif [ "$tgerr" == true ]; then
-		killall cbot.exe && xfce4-terminal -e "./cbot.exe"
-		tgerr = false
-        fi
+	fi
 done
 }
 
