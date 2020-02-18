1c075b590fb0104ced297d7b16ab2c256684c2e4
root <root@vps34358ua.hyperhost.name>
2020-02-18 03:27:14 -0500

    v1.0, release hf7

--- bot.sh
+++ bot.sh
@@ -4,13 +4,13 @@ cbot(){
 ./start.py
 }
 
-echo "Start of cbot"
-
 while true; do
 	if [ `date | cut -d" " -f 4 | cut -d":" -f 2` = 00 ]; then
-			killall python3; cbot && echo "Timebased restart of bot occured"
+			echo "Timebased restart of bot occured"
+			killall python3; cbot
 	elif [ -z `pgrep python3` ]; then
-		cbot && echo "Start of bot occured, whether it was connection error or first time of start!"
+		echo "Start of bot occured, whether it was connection error or first time of start!"
+		cbot
 	fi
 	sleep 10
 done
