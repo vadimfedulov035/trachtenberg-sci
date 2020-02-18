b85b65217b281f267d1949283d9ccb0b53c4db45
root <root@vps34358ua.hyperhost.name>
2020-02-18 11:44:38 -0500

    v1.0, release hf10

--- bot.sh
+++ bot.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cbot(){
-./start.py
+exec "./start.py"
 }
 
 while true; do
-	if [ `date | cut -d" " -f 4 | cut -d":" -f 2` = 00 ]; then
+	if [ `date | cut -d" " -f 4 | cut -d":" -f 2` = "00" ]; then
 			echo "Timebased restart of bot occured"
 			killall python3; cbot
 	elif [ -z `pgrep python3` ]; then
