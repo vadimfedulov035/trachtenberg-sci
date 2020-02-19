2f1235ce222cf5d6868a9b5932e61d23f91640dd
root <root@vps34358ua.hyperhost.name>
2020-02-19 04:34:12 -0500

    v1.1, stable release hf2

--- init.sh
+++ init.sh
@@ -7,11 +7,11 @@ sh -c "$(./start.py)" &
 while true; do
 	if [ `date | cut -d" " -f 4 | cut -d":" -f 2` = "00" ]; then
 			killall python3.7
-			cbot
 			echo "Timebased restart of bot occured"
+			cbot
 	elif [ -z `pgrep python3.7` ]; then
-		cbot
 		echo "Start of bot occured, whether it was reconnection or first start"
+		cbot
 	fi
 	sleep 35
 done
