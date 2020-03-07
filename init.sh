181c03169de39f6a97a69e7c2640cb08d6b7cd19
root <root@vps34358ua.hyperhost.name>
2020-03-07 13:50:08 -0500

    v1.3, official release stable version hflast

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 
 #!/bin/sh
 
-cbot(){
-sh -c "$(./start.py)"  &
+pbot(){
+sh -c "$(pypy3 start.py)"  &
 }
 
-killall python3.7
+killall pypy3
 
 until [ `date | cut -d" " -f 5 | cut -d":" -f 3` = 00 ]; do
 	sleep 1
@@ -13,12 +13,12 @@ done
 
 while true; do
 	if [ `date | cut -d" " -f 5 | cut -d":" -f 1` = 12 ] && [ `date | cut -d" " -f 5 | cut -d":" -f 2` = 00 ]; then
-			killall python3.7
-			cbot
+			killall pypy3
+			pbot
 			echo "Timebased restart of bot occured"
-	elif [ -z `pgrep python3.7` ]; then
-		killall python3.7
-		cbot
+	elif [ -z `pgrep pypy3` ]; then
+		killall pypy3
+		pbot
 		echo "Start of bot occured, whether it was reconnection or first start"
 	fi
 	sleep 60
--- start.py
+++ start.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3.7
 import asyncio
 from bot import Bot
 
@@ -33,80 +32,6 @@ pbot22 = Bot(token, 22)
 pbot23 = Bot(token, 23)
 pbot24 = Bot(token, 24)
 pbot25 = Bot(token, 25)
-pbot26 = Bot(token, 26)
-pbot27 = Bot(token, 27)
-pbot28 = Bot(token, 28)
-pbot29 = Bot(token, 29)
-pbot30 = Bot(token, 30)
-pbot31 = Bot(token, 31)
-pbot32 = Bot(token, 32)
-pbot33 = Bot(token, 33)
-pbot34 = Bot(token, 34)
-pbot35 = Bot(token, 35)
-pbot36 = Bot(token, 36)
-pbot37 = Bot(token, 37)
-pbot38 = Bot(token, 38)
-pbot39 = Bot(token, 39)
-pbot40 = Bot(token, 40)
-pbot41 = Bot(token, 41)
-pbot42 = Bot(token, 42)
-pbot43 = Bot(token, 43)
-pbot44 = Bot(token, 44)
-pbot45 = Bot(token, 45)
-pbot46 = Bot(token, 46)
-pbot47 = Bot(token, 47)
-pbot48 = Bot(token, 48)
-pbot49 = Bot(token, 49)
-pbot50 = Bot(token, 50)
-pbot51 = Bot(token, 51)
-pbot52 = Bot(token, 52)
-pbot53 = Bot(token, 53)
-pbot54 = Bot(token, 54)
-pbot55 = Bot(token, 55)
-pbot56 = Bot(token, 56)
-pbot57 = Bot(token, 57)
-pbot58 = Bot(token, 58)
-pbot59 = Bot(token, 59)
-pbot60 = Bot(token, 60)
-pbot61 = Bot(token, 61)
-pbot62 = Bot(token, 62)
-pbot63 = Bot(token, 63)
-pbot64 = Bot(token, 64)
-pbot65 = Bot(token, 65)
-pbot66 = Bot(token, 66)
-pbot67 = Bot(token, 67)
-pbot68 = Bot(token, 68)
-pbot69 = Bot(token, 69)
-pbot70 = Bot(token, 60)
-pbot71 = Bot(token, 71)
-pbot72 = Bot(token, 72)
-pbot73 = Bot(token, 73)
-pbot74 = Bot(token, 74)
-pbot75 = Bot(token, 75)
-pbot76 = Bot(token, 76)
-pbot77 = Bot(token, 77)
-pbot78 = Bot(token, 78)
-pbot79 = Bot(token, 79)
-pbot80 = Bot(token, 80)
-pbot81 = Bot(token, 81)
-pbot82 = Bot(token, 82)
-pbot83 = Bot(token, 83)
-pbot84 = Bot(token, 84)
-pbot85 = Bot(token, 85)
-pbot86 = Bot(token, 86)
-pbot87 = Bot(token, 87)
-pbot88 = Bot(token, 88)
-pbot89 = Bot(token, 89)
-pbot90 = Bot(token, 90)
-pbot91 = Bot(token, 91)
-pbot92 = Bot(token, 92)
-pbot93 = Bot(token, 93)
-pbot94 = Bot(token, 94)
-pbot95 = Bot(token, 95)
-pbot96 = Bot(token, 96)
-pbot97 = Bot(token, 97)
-pbot98 = Bot(token, 98)
-pbot99 = Bot(token, 99)
 
 
 async def main():
@@ -137,81 +62,8 @@ async def main():
         pbot22.start(),
         pbot23.start(),
         pbot24.start(),
-        pbot25.start(),
-        pbot26.start(),
-        pbot27.start(),
-        pbot28.start(),
-        pbot29.start(),
-        pbot30.start(),
-        pbot31.start(),
-        pbot32.start(),
-        pbot33.start(),
-        pbot34.start(),
-        pbot35.start(),
-        pbot36.start(),
-        pbot37.start(),
-        pbot38.start(),
-        pbot39.start(),
-        pbot40.start(),
-        pbot41.start(),
-        pbot42.start(),
-        pbot43.start(),
-        pbot44.start(),
-        pbot45.start(),
-        pbot46.start(),
-        pbot47.start(),
-        pbot48.start(),
-        pbot49.start(),
-        pbot50.start(),
-        pbot51.start(),
-        pbot52.start(),
-        pbot53.start(),
-        pbot54.start(),
-        pbot55.start(),
-        pbot56.start(),
-        pbot57.start(),
-        pbot58.start(),
-        pbot59.start(),
-        pbot60.start(),
-        pbot61.start(),
-        pbot62.start(),
-        pbot63.start(),
-        pbot64.start(),
-        pbot65.start(),
-        pbot66.start(),
-        pbot67.start(),
-        pbot68.start(),
-        pbot69.start(),
-        pbot70.start(),
-        pbot71.start(),
-        pbot72.start(),
-        pbot73.start(),
-        pbot74.start(),
-        pbot75.start(),
-        pbot76.start(),
-        pbot77.start(),
-        pbot78.start(),
-        pbot79.start(),
-        pbot80.start(),
-        pbot81.start(),
-        pbot82.start(),
-        pbot83.start(),
-        pbot84.start(),
-        pbot85.start(),
-        pbot86.start(),
-        pbot87.start(),
-        pbot88.start(),
-        pbot89.start(),
-        pbot90.start(),
-        pbot91.start(),
-        pbot92.start(),
-        pbot93.start(),
-        pbot94.start(),
-        pbot95.start(),
-        pbot96.start(),
-        pbot97.start(),
-        pbot98.start(),
-        pbot99.start()
+        pbot25.start()
         )
 
-asyncio.run(main())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
