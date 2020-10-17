d51030fc031004809f242fbfc4d7f1694adb1810
vadimfedulov035 <72294724+vadimfedulov035@users.noreply.github.com>
2020-10-17 14:22:25 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,12 +7,7 @@ cbotf(){
 	./cbot &
 }
 
-killf(){
-	killall cidc cbot
-}
-
 while true; do
-	[ `date | cut -d" " -f 4 | cut -d":" --fields 1,2` = 00:00 ] && [ `date | cut -d" " -f 1` = "Fri" ] && killf && cbotf && echo "Restart: `date`"
 	[ -z `pgrep cidc` ] || [ -z `pgrep cbot` ] && cbotf && echo "Restart: `date`"
 	sleep 0.1
 done
